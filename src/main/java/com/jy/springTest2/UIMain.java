9c795b5e3725e0d7ff92ccb5f312a2c82d43c9b5
Juyoung Kim <kimmykim.jy@gmail.com>
2022-06-29 15:45:22 +0900

    first commit

---
+++ src/main/java/com/jy/springTest2/UIMain.java
@@ -0,0 +1,11 @@
+package com.jy.springTest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UIMain {
+    public static void main(String arg[]) {
+        SpringApplication.run(UIMain.class, arg);
+    }
+}
---
+++ src/main/java/com/jy/springTest2/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.jy.springTest2.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String hello() {
+        return "hello world";
+    }
+}
+
+
+
